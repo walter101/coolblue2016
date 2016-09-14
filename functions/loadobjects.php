537c341cc05c04b5001be3e9fe7e9644ac434b3f
walter101 <walterpothof@gmail.com>
2016-09-14 14:00:45 +0200

    Update loadobjects.php

--- functions/loadobjects.php
+++ functions/loadobjects.php
@@ -1,9 +1,9 @@
 <?php
 
 defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
-defined('DB_USER') ? null : define("DB_USER", "waltepf108_DOnot");
-defined('DB_PASS') ? null : define("DB_PASS", "test5MrS771");
-defined('DB_NAME') ? null : define("DB_NAME", "waltepf108_coolblue");
+defined('DB_USER') ? null : define("DB_USER", "xxx");
+defined('DB_PASS') ? null : define("DB_PASS", "xxx");
+defined('DB_NAME') ? null : define("DB_NAME", "xxx");
 
 defined('ROOTDIR') ? null : define('ROOTDIR','/home/waltepf108/domains/walterpothof.nl/public_html/coolblue/');
 
