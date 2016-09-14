363737ca551dc5d1b14f7f0b836dfebdb9eeeffe
walter101 <walterpothof@gmail.com>
2016-09-14 11:41:35 +0200

    Add files via upload

---
+++ functions/loadtemplates.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "templates/show_tablet_template.php";
+require_once "templates/footer.php";
+require_once "templates/glimlach.php";
+require_once "templates/winkelwagen_showproduct.php";
+require_once "templates/productpage_show_product.php";
+
+
+?>
+
